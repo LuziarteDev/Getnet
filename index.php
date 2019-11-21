79c1d629146755f73f4121e09c80443b363dd632
Jose Luziarte <joseyesfox@gmail.com>
2019-11-21 02:47:54 -0300

    Teste pagamento

--- index.php
+++ index.php
@@ -14,5 +14,9 @@ $getnet = new Getnet(
 );
 
 $result = $getnet->getToken();
+$test = base64_encode('<script id="getnet-loader-script" src="https://checkout-sandbox.getnet.com.br/loader.js" data-getnet-sellerid="055b2577-4be6-4c1b-af6b-f77d44b672f2" data-getnet-token="Bearer 478a2a27-87cb-403f-95a5-abf8eb2db51b" data-getnet-payment-methods-disabled="[]" data-getnet-amount="123000.45" data-getnet-customerid="12345" data-getnet-orderid="12345" data-getnet-button-class="pay-button-getnet" data-getnet-customer-first-name="João" data-getnet-customer-last-name="da Silva" data-getnet-customer-document-type="CPF" data-getnet-customer-document-number="82916868070" data-getnet-customer-email="teste@teste.com" data-getnet-customer-address-street="Av. Nações Unidas" data-getnet-customer-address-street-number="1" data-getnet-customer-address-neighborhood="Brooklin" data-getnet-customer-address-city="São Paulo" data-getnet-customer-address-state="SP" data-getnet-customer-address-zipcode="04578907" data-getnet-customer-country="Brasil" data-getnet-shipping-address="[]" data-getnet-items="[{"name":"","description":"","value":0,"quantity":0,"sku":""}]data-getnet-recurrency="true""></script>');
 
-var_dump($result);
+
+echo('<button class="pay-button-getnet">Realizar pagamento</button>');
+
+echo( base64_decode( $test ) );
